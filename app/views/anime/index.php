23fce6756d3a8b5a497a24b6853e753bbfb522d8
bagas003 <bagasaryo03@gmail.com>
2023-10-03 11:47:46 +0700

    anime page

--- app/views/anime/index.php
+++ app/views/anime/index.php
@@ -1,11 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+<?php include '../includes/header.php'; ?>
+
 <body>
-  <h1> Ini Anime Info </h1>
+    <div class="anime-container">
+        <div class="anime-header">
+            <h1>Shingeki no Kyojin (Attack on Titan)</h1>
+        </div>
+
+        <div class="anime-content">
+            <div class="anime-image">
+                <img src="../../public/img/placeholder.jpg" alt="Anime Cover Image">
+            </div>
+
+            <div class="anime-details">
+                <div>Score: <span>8.58</span></div>
+                <div>Type: <span>TV</span></div>
+                <div>Status: <span>Completed</span></div>
+                <div>Genres: <span>Action, Drama, Thriller</span></div>
+                <div>Studios: <span>Wit Studio</span></div>
+                <div>Aired: <span>Apr 7, 2013 to Sep 29, 2013</span></div>
+                <div>Episodes: <span>25</span></div>
+                <button class="btn">Add MyList</button>
+            </div>
+
+            <div class="anime-trailer">
+                <h2>Trailer</h2>
+                <iframe width="560" height="315" src="path-to-trailer" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        
+        <div class="synopsis">
+            <h2>Sinopsis:</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget nisl ex. Integer tempor turpis vitae massa euismod imperdiet. Nulla porta sagittis mauris, nec tempor magna semper nec. Fusce laoreet mattis eros, ut consectetur nisl condimentum aliquam. Integer at erat ut nibh vestibulum vulputate.</p> <!-- Add the synopsis content here -->
+        </div>
+        
+        
+        <h2>Anime Review</h2>
+        <!-- anime-review Item Loop -->
+        <div class="anime-review-list">
+            <!-- This is a placeholder; repeat this for each anime-review item -->
+            <div class="anime-review-item">
+                <div class="user-info">
+                    <span class="username">Username123</span>
+                    <small>Date: 2023-10-04</small>
+                    <div class="rating">Rating: 10/10★</div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget nisl ex. Integer tempor turpis vitae massa euismod imperdiet. Nulla porta sagittis mauris, nec tempor magna semper nec. Fusce laoreet mattis eros, ut consectetur nisl condimentum aliquam. Integer at erat ut nibh vestibulum vulputate.</p>
+            </div>
+            <div class="anime-review-item">
+                <div class="user-info">
+                    <span class="username">Username123</span>
+                    <small>Date: 2023-10-04</small>
+                    <div class="rating">Rating: 10/10★</div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget nisl ex. Integer tempor turpis vitae massa euismod imperdiet. Nulla porta sagittis mauris, nec tempor magna semper nec. Fusce laoreet mattis eros, ut consectetur nisl condimentum aliquam. Integer at erat ut nibh vestibulum vulputate.</p>
+            </div>
+            <div class="anime-review-item">
+                <div class="user-info">
+                    <span class="username">Username123</span>
+                    <small>Date: 2023-10-04</small>
+                    <div class="rating">Rating: 10/10★</div>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget nisl ex. Integer tempor turpis vitae massa euismod imperdiet. Nulla porta sagittis mauris, nec tempor magna semper nec. Fusce laoreet mattis eros, ut consectetur nisl condimentum aliquam. Integer at erat ut nibh vestibulum vulputate.</p>
+            </div>
+        </div>
+            
+
+    </div>
 </body>
-</html>
+
+<?php include '../includes/footer.php'; ?>
